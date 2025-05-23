d6c493adc3f5bd78c459d3beff194d9e1f5b520b
bipasha3265 <bipasham585@gmail.com>
2025-05-23 20:12:09 +0530

    ATM_INTERFACE.java

---
+++ ATM_INTERFACE.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class BankAccount{
+  private double balance;
+  public BankAccount(double IB){
+    balance = IB; //initial balance (IB)
+    public double getBalance(){
+      return
+    
